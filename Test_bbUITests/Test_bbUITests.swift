656fe3d1a5c594d923fb52bbff1f2da5be561802
Frane Poljak <frane.poljak@strainprint.ca>
2020-10-31 18:02:43 +0100

    Finish UI tests

--- Test_bbUITests/Test_bbUITests.swift
+++ Test_bbUITests/Test_bbUITests.swift
@@ -51,35 +51,46 @@ class Test_bbUITests: XCTestCase {
             
             XCTAssertEqual(label.label, "Title text")
             
-            textColorButton.tap()
-            
-            let label = self.app.staticTexts.matching(identifier: "colorpicker_title_label").firstMatch
-            let cells = self.app.collectionViews.cells //.matching(identifier: "ColorPickerCollectionViewCell") // doesn't work with identifier!
-            let firstCell = cells.firstMatch
-            
-            self.expectation(for: exists, evaluatedWith: firstCell, handler: nil)
-            
-            self.waitForExpectations(timeout: 1.0, handler: { error in
-                guard error == nil else {
-                    return
-                }
-                XCTAssertEqual(label.label, "Odaberi boju teksta")
-                XCTAssertEqual(cells.count, 5, "There should be 5 colors to choose from")
+            var testColorPicker: ((XCUIElement, Bool) -> Void)!
+            testColorPicker = { [unowned self] (button: XCUIElement, isText: Bool) -> Void in
+                button.tap()
                 
-                cells.element(boundBy: Int.random(in: 0...4)).tap() // tap on any cell, beehaviour should be identical (we don't test colors here
+                let label = self.app.staticTexts.matching(identifier: "colorpicker_title_label").firstMatch
+                let cells = self.app.collectionViews.cells
+                let firstCell = cells.firstMatch
                 
-            })
+                self.expectation(for: exists, evaluatedWith: firstCell, handler: nil)
+                
+                self.waitForExpectations(timeout: 1.0, handler: { error in
+                    guard error == nil else {
+                        return
+                    }
+                    XCTAssertEqual(label.label, "Odaberi boju \(isText ? "teksta" : "pozadine")")
+                    XCTAssertEqual(cells.count, 5, "There should be 5 colors to choose from")
+                    
+                    cells.element(boundBy: Int.random(in: 0...4)).tap() // tap on any cell, beehaviour should be identical (we don't test colors here
+                    
+                    if isText {
+                        self.expectation(for: exists, evaluatedWith: button, handler: nil)
+                        self.waitForExpectations(timeout: 1.0, handler: { error in
+                            testColorPicker(backgroundColorButton, false)
+                        })
+                    }
+                })
+            }
+            
+            testColorPicker(textColorButton, true)
         }
     }
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }
 
 extension HttpServer {
